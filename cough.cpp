4fbd32f03e0784348a8f775b308aa8a726243bd2
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-12-04 00:02:28 -0300

    Dummy change in 1.0 branch.
    Add newline to cough.cpp output.

--- cough.cpp
+++ cough.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(void)
 {
-  cout<<"***Cough***, sorry all xD. Test branch after speed fix";
+  cout<<"***Cough***, sorry all xD. Test branch after speed fix" << endl;
   return 0;
 }
