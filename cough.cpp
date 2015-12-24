1aea5fa5ce6f408656b203e2b01cbfac6da115c9
Ben Cooksley <bcooksley@kde.org>
2015-12-24 20:34:58 +1300

    Revert "Testing spring.kde.org"
    This reverts commit 8389be493a4735628461d5cf431da23e2d2d44dd.

---
+++ cough.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  cout << "receive in stdin" << endl;
+  cout<<"***Cough***, sorry all xD. Test branch after speed fix" << endl;
+  return 0;
+}
