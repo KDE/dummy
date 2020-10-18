ae08e8ff187e0277199ee0051d2f475100b64577
Ben Cooksley <bcooksley@kde.org>
2020-10-18 17:49:31 +1300

    Next round

--- Messages.sh
+++ Messages.sh
@@ -4,6 +4,6 @@
 # more blah
 # More testing
 # Testing round 3
-# Make sure Neon works
+# Make sure Neon works - next round of checks
 
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/dummy.pot
