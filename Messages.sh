fe7636d7cd2c1a90f865bf9519fe602e37519ec8
Ben Cooksley <bcooksley@kde.org>
2020-10-18 17:53:45 +1300

    Let's try this way

--- Messages.sh
+++ Messages.sh
@@ -4,6 +4,6 @@
 # more blah
 # More testing
 # Testing round 3
-# Make sure Neon works - next round of checks...
+# Make sure Neon works - next round of checks - should work now?
 
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/dummy.pot
