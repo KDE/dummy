65ec4600d5c60fab16afd12138d543a9a0942233
Ben Cooksley <bcooksley@kde.org>
2021-01-17 16:18:02 +1300

    Next round

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import wsgiref.simple_server
 import WebResources
 
 # Testing
-# Test 3
+# Test 4
 
 # Gather the command line arguments we need
 parser = argparse.ArgumentParser(description='Specialist redirector to handle commits.kde.org permanent links')
